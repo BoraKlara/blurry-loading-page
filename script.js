baa62cb70635846cef6bf82bd05dfea48fa02dd6
B. Klara Simon <borbala.klara@gmail.com>
2021-05-19 23:34:43 +0200

    debugging js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const loadText = document.querySelector(".loading__text");
-const gb = document.querySelector(".bg");
+const bg = document.querySelector(".bg");
 
 let load = 0;
 
@@ -15,10 +15,11 @@ function blurring() {
   loadText.innerText = `${load}%`;
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
 
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-  /*  overlay.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`; */
+  bg.style.filter = `blur(${sclae(load, 0, 100, 30, 0)}px)`;
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
+//Photo by <a href="https://unsplash.com/@heytowner?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">JOHN TOWNER</a> on <a href="https://unsplash.com/s/photos/forest?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
